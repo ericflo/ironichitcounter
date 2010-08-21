a67853ea811bddb7101d45a9cb10929c5c48c520
Eric Florenzano <floguy@gmail.com>
2010-08-21 13:19:54 -0700

    Hook up homepage

--- hitcounter.py
+++ hitcounter.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
